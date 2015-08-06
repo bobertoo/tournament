124ba9d48e6f0e30b8fb49dc1941db44e3d12cea
Vinny Martinez <vincentemartinez@vdawg.local>
2015-08-06 17:59:33 -0400

    added comments to tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -1,10 +1,17 @@
+-- this creates the tournament database
 CREATE DATABASE tournament;
+-- this puts us into the new database
 \c tournament;
+-- this creates a table for players with a name and unique id number
 CREATE TABLE players( id SERIAL, name TEXT);
+-- this creates a table for matches. it has 2 players and shows winner and loser
 CREATE TABLE matches( winner INTEGER, loser INTEGER);
-CREATE VIEW wins as select players.id, count(matches.winner) as wins from players left join matches on players.id = matches.winner group by players.id;
-CREATE VIEW matches_played as select players.id, count(matches.winner + matches.loser) as nummatches from players left join matches on players.id = matches.winner or players.id = matches.loser group by players.id;
-CREATE VIEW standings as select players.id, players.name, wins.wins, matches_played.nummatches from players, wins, matches_played where players.id = wins.id and players.id = matches_played.id group by players.id, players.name, wins.wins, matches_played.nummatches order by wins.wins desc;
+-- created a view to show player id with associated wins
+CREATE VIEW wins AS SELECT players.id, count(matches.winner) AS wins FROM players LEFT JOIN matches ON players.id = matches.winner GROUP BY players.id;
+-- created a view to show player id with number of matches played
+CREATE VIEW matches_played AS SELECT players.id, count(matches.winner + matches.loser) AS nummatches FROM players LEFT JOIN matches ON players.id = matches.winner OR players.id = matches.loser GROUP BY players.id;
+-- created a view that shows player id, name, number of wins and matches played
+CREATE VIEW standings AS SELECT players.id, players.name, wins.wins, matches_played.nummatches FROM players, wins, matches_played WHERE players.id = wins.id AND players.id = matches_played.id GROUP BY players.id, players.name, wins.wins, matches_played.nummatches ORDER BY wins.wins DESC;
 -- Table definitions for the tournament project.
 --
 -- Put your SQL 'create table' statements in this file; also 'create view'
