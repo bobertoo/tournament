e56bf0bfbddedce2dbf7d8cc575f4a31f80a0bef
Vinny Martinez <vincentemartinez@vdawg.local>
2015-08-11 00:09:12 -0400

    update code with formatting better swiss pairing

--- tournament.py
+++ tournament.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 # 
 # tournament.py -- implementation of a Swiss-system tournament
-#
+# it will add and remove players show wins, 
+#  number of matches played, standings and will match with another player
+
 #imported the python psql plugin
 import psycopg2
 
 #connecting to db
 def connect():
-    """Connect to the PostgreSQL database.  Returns a database connection."""
     return psycopg2.connect("dbname=tournament")
 
 #removing all matches
 def deleteMatches():
-    """Remove all the match records from the database."""
     DB = connect()
     c = DB.cursor()
     c.execute("DELETE FROM matches;")
@@ -21,7 +21,6 @@ def deleteMatches():
 
 #removing all players
 def deletePlayers():
-    """Remove all the player records from the database."""
     DB = connect()
     c = DB.cursor()
     c.execute("DELETE FROM players;")
@@ -30,7 +29,6 @@ def deletePlayers():
 
 #adding player count up
 def countPlayers():
-    """Returns the number of players currently registered."""
     DB = connect()
     c = DB.cursor()
     c.execute("SELECT count(*) FROM players")
@@ -40,14 +38,6 @@ def countPlayers():
 
 #adding players
 def registerPlayer(name):
-    """Adds a player to the tournament database.
-  
-    The database assigns a unique serial id number for the player.  (This
-    should be handled by your SQL database schema, not in your Python code.)
-  
-    Args:
-      name: the player's full name (need not be unique).
-    """
     DB = connect()
     c = DB.cursor()
     c.execute("INSERT INTO players (name) VALUES (%s);", (name,))
@@ -56,18 +46,6 @@ def registerPlayer(name):
 
 #viewing the player standings
 def playerStandings():
-    """Returns a list of the players and their win records, sorted by wins.
-
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
-
-    Returns:
-      A list of tuples, each of which contains (id, name, wins, matches):
-        id: the player's unique id (assigned by the database)
-        name: the player's full name (as registered)
-        wins: the number of matches the player has won
-        matches: the number of matches the player has played
-    """
     DB = connect()
     c = DB.cursor()
     c.execute("select * from standings;")
@@ -77,12 +55,6 @@ def playerStandings():
 
 #adding winners and losers of matches
 def reportMatch(winner, loser):
-    """Records the outcome of a single match between two players.
-
-    Args:
-      winner:  the id number of the player who won
-      loser:  the id number of the player who lost
-    """
     DB = connect()
     c = DB.cursor()
     c.execute("INSERT INTO matches(winner, loser) VALUES (%s, %s);", (winner, loser,))
@@ -92,25 +64,19 @@ def reportMatch(winner, loser):
 #used the testPairings function to fill the swissPairing matching requirements 
 #this pairs players my determining who one previous game
 def swissPairings():
-    """Returns a list of pairs of players for the next round of a match.
-  
-    Assuming that there are an even number of players registered, each player
-    appears exactly once in the pairings.  Each player is paired with another
-    player with an equal or nearly-equal win record, that is, a player adjacent
-    to him or her in the standings.
-  
-    Returns:
-      A list of tuples, each of which contains (id1, name1, id2, name2)
-        id1: the first player's unique id
-        name1: the first player's name
-        id2: the second player's unique id
-        name2: the second player's name
-    """
+    i = 0
+    j = []
     standings = playerStandings()
-    p1 = standings[0][0:2]
-    p2 = standings[1][0:2]
-    p3 = standings[2][0:2]
-    p4 = standings[3][0:2]
-    p1and2 = (p1 + p2)
-    p3and4 = (p3 + p4)
-    return p1and2, p3and4
+    #added the if statment to check if game had even or odd number
+    if len(standings) % 2 != 0:
+        print 'we want an even number of players plz' 
+    else:
+        while i < len(standings):
+            a = standings[i][0:2]
+            i += 1
+            b = standings[i][0:2]
+            i += 1
+	    j += [(a + b)]
+        print j
+        return j
+
--- tournament.sql
+++ tournament.sql
@@ -1,23 +1,43 @@
--- this creates the tournament database
-CREATE DATABASE tournament;
--- this puts us into the new database
-\c tournament;
--- this creates a table for players with a name and unique id number
-CREATE TABLE players( id SERIAL, name TEXT);
--- this creates a table for matches. it has 2 players and shows winner and loser
-CREATE TABLE matches( winner INTEGER, loser INTEGER);
--- created a view to show player id with associated wins
-CREATE VIEW wins AS SELECT players.id, count(matches.winner) AS wins FROM players LEFT JOIN matches ON players.id = matches.winner GROUP BY players.id;
--- created a view to show player id with number of matches played
-CREATE VIEW matches_played AS SELECT players.id, count(matches.winner + matches.loser) AS nummatches FROM players LEFT JOIN matches ON players.id = matches.winner OR players.id = matches.loser GROUP BY players.id;
--- created a view that shows player id, name, number of wins and matches played
-CREATE VIEW standings AS SELECT players.id, players.name, wins.wins, matches_played.nummatches FROM players, wins, matches_played WHERE players.id = wins.id AND players.id = matches_played.id GROUP BY players.id, players.name, wins.wins, matches_played.nummatches ORDER BY wins.wins DESC;
--- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
+DROP DATABASE IF EXISTS tournament;
 
+CREATE DATABASE tournament;
+
+\c tournament;
+
+CREATE TABLE players(
+  id SERIAL PRIMARY KEY,
+ name TEXT
+);
+
+CREATE TABLE matches(
+  id SERIAL PRIMARY KEY,
+ winner INTEGER REFERENCES players(id),
+ loser INTEGER REFERENCES players(id)
+);
+
+CREATE VIEW wins
+  AS SELECT players.id, count(matches.winner)
+ AS wins FROM players
+ LEFT JOIN matches
+ ON players.id = matches.winner
+ GROUP BY players.id
+;
+
+CREATE VIEW matches_played
+  AS SELECT players.id, count(matches.winner + matches.loser)
+ AS nummatches FROM players
+ LEFT JOIN matches
+ ON players.id = matches.winner
+ OR players.id = matches.loser
+ GROUP BY players.id
+;
+
+CREATE VIEW standings
+  AS SELECT players.id, players.name, wins.wins, matches_played.nummatches
+ FROM players, wins, matches_played
+ WHERE players.id = wins.id
+ AND players.id = matches_played.id
+ GROUP BY players.id, players.name, wins.wins, matches_played.nummatches
+ ORDER BY wins.wins DESC
+;
 
